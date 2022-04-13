eb7fcae01195971758d34fb664496cc6b47ccf4d
Alex-sys-sys <94735806+Alex-sys-sys@users.noreply.github.com>
2022-04-14 00:58:33 +0300

    login form

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ api.add_resource(Comment_resource, '/api/v1/comment/<int:comment_id>')
 api.add_resource(Tag_post_resource, '/api/v1/tag/<int:tag_id>')
 
 
-
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
@@ -63,6 +62,23 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect('/')
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html',
+                           title='Авторизация',
+                           form=form)
 
 
 def main():
